9d499cb9aec51ef1c35dfe92686cdde5dadd1361
sakibtamim <sakib@purrfecthq.com>
2026-06-10 19:10:00 +0600

    fix(linux): use tauri native pacman bundler and resolve AppImage white screen

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    #[cfg(target_os = "linux")]
+    {
+        std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+        std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+    }
     purrfect_download_manager_lib::run();
 }
